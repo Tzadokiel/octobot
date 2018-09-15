183415bd026351cf23a2d2aef93bc6d3929428a8
Tzadokiel <36882788+gregoirebrun@users.noreply.github.com>
2018-09-15 17:46:09 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,4 +1,3 @@
-
 const Discord = require('discord.js'); // Récupération de la bibliothèque discord
 const bot = new Discord.Client(); // Création du client bot discord
 const prefix = '::'; // Préfixe utiliser pour les commandes
@@ -14,12 +13,12 @@ bot.on('ready', () => {
 });
 
 bot.on('guildMemberAdd', member =>{
-    let role = member.guild.roles.find(roles => roles.name, "Mousse");
+    let role = member.guild.roles.find("name", "Mousse");
     const embed = new Discord.RichEmbed()
     .setTitle('Nouvel Arrivant')
     .setColor(0x000F84)
     .addField(`Hey ! Bienvenue à bord ${member} ! Tu es ici sur le serveur dicord de la communauté ${member.guild.name}, tu es invité à lire le #reglement et à faire une petite afin que l'on te connaisse un peu plus !`)
-    member.guild.channels.find(channel => channel.name, "accueil").send(embed);
+    member.guild.channels.find("name", "accueil").send(embed);
     member.addRole(role);
 });
 
@@ -33,36 +32,40 @@ bot.on('message', message => {
         .setTitle('Help')
         .setColor(0x000F84)
         .setDescription('Recapitulatif des Commandes d\'Octobot !')
-        .addField("Commande Attribution Rank" , "Overwatch ::overwatch");
+        .addField("Commande Attribution Rank" , "Les commandes pour s'attribuer ou retirer un jeu est *::game* suivit du jeu (**::jeux** pour afficher les jeux disponibles), les majuscules ne sont pas prises en compte")
+        .addField("Exemple :", "::game overwatch");
         message.channel.send(helpEmbed);
     }
 
     // Aquisition et perte des ranks
-        var listeRole = [
-            "Rocket League",
-            "League of Legend",
-            "For Honor",
-            "Overwatch",
-            "PUBG",
-            "Paladin",
-            "Fortnite",
-            "World of Warcraft",
-            "CSGO",
-            "Brawlhalla",
-            "Battlefield 1",
-            "Monster Hunter World",
-            "Hearthstone",
-            "GTA V",
-            "Rainbow 6 : Siege",
-            "War Thunder",
-            "Mousse"
-        ];
+    var listeRole = [
+        "Rocket League",
+        "League of Legend",
+        "For Honor",
+        "Overwatch",
+        "PUBG",
+        "Paladin",
+        "Fortnite",
+        "World of Warcraft",
+        "CSGO",
+        "Brawlhalla",
+        "Battlefield 1",
+        "Monster Hunter World",
+        "Hearthstone",
+        "GTA V",
+        "Rainbow 6 : Siege",
+        "War Thunder",
+        "Mousse"
+    ];
 
+    var reussite = 0;
+
+    if(message.content.split(" ")[0] === prefix + "game"){
         listeRole.forEach(function(element){
             if(message.content.toLowerCase() === prefix + "game " + element.toLowerCase()){
-                let roleAssign = message.member.guild.roles.find(role => role.name, element);
-                
-                if(!message.member.roles.find(roles => roles.name, element)){
+                reussite = 1;
+                let roleAssign = message.member.guild.roles.find("name", element);
+                if(!message.member.roles.find("name", element)){
                     var roleEmbed = new Discord.RichEmbed()
                     .setTitle('Jeu Ajouté')
                     .setColor(0x00a51b)
@@ -73,14 +76,44 @@ bot.on('message', message => {
                 }else{
                     var roleEmbed = new Discord.RichEmbed()
                     .setTitle('Jeu Retiré')
-                    .setColor(0xfc0043)
+                    .setColor(0xdd6300)
                     .setAuthor("Assignation des Jeux", message.author.avatarURL)
                     .setDescription(`Pale sans bleu ! Tu as été retiré de la liste des joueurs de **${element}** !!`);
                     message.channel.send(roleEmbed);
                     message.member.removeRole(roleAssign);
                 }
+            }else{
+                if(reussite === 0){
+                    reussite = 2;
+                }
             }
         });
+    }
+
+    if(reussite === 2){
+        var roleEmbed = new Discord.RichEmbed()
+        .setTitle('Erreur')
+        .setColor(0xfc0043)
+        .setAuthor("Assignation des Jeux", message.author.avatarURL)
+        .setDescription(`Attention le jeu **${message.content.split(" ")[1]}** n'éxiste pas !! Utilisez la commande **::jeux** pour connaitre les jeux présents sur le discord`);
+        message.channel.send(roleEmbed);
+    }
+
+    var listeAfficheJeux = "";
+    listeRole.forEach(function(element){
+        listeAfficheJeux += "\n" + element;
+    });
+    
+    if(message.content === prefix + "jeux"){
+        var roleEmbed = new Discord.RichEmbed()
+        .setTitle('Jeux Disponibles')
+        .setColor(0x000F84)
+        .setDescription(`Voici la liste des jeux disponibles sur ${message.guild.name} : ${listeAfficheJeux}`);
+        message.channel.send(roleEmbed)
+    }
+    
 });
+
+bot.login(token);
 
 bot.login(process.env.TOKEN);
