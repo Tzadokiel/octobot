1685b103408f0f310b1117b50aa69bf94427461e
Tzadokiel <36882788+gregoirebrun@users.noreply.github.com>
2018-09-15 18:54:26 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -17,7 +17,7 @@ bot.on('guildMemberAdd', member =>{
     const embed = new Discord.RichEmbed()
     .setTitle('Nouvel Arrivant')
     .setColor(0x000F84)
-    .addField(`Hey ! Bienvenue à bord ${member} ! Tu es ici sur le serveur dicord de la communauté ${member.guild.name}, tu es invité à lire le #reglement et à faire une petite afin que l'on te connaisse un peu plus !`)
+    .addField("Bienvenue à bord !!",`Hey ! Salut à toi ${member} ! Tu es ici sur le serveur dicord de la communauté ${member.guild.name}, tu es invité à lire le #reglement et à faire une petite afin que l'on te connaisse un peu plus !`)
     member.guild.channels.find("name", "accueil").send(embed);
     member.addRole(role);
 });
