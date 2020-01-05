4a8c20677b53d2233025836bedb8c03215a8d01e
Tzadokiel <36882788+gregoirebrun@users.noreply.github.com>
2020-01-05 11:30:13 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -15,7 +15,7 @@ bot.on('ready', () => {
 });
 
 bot.on('guildMemberAdd', member =>{
-    var role = member.guild.roles.find("name", "Docker");
+    var role = member.guild.roles.find('name', 'Docker');
     var reglement = member.guild.channels.find("name", "reglement");
     var presentation = member.guild.channels.find("name", "presentation");
     const embed = new Discord.RichEmbed()
